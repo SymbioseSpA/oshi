de01940a8783f2bd441395adf4e9213786270b5e
Francisco Collao Garate <francisco.collao@symbiose.cl>
2016-04-07 11:49:04 -0300

    CHG: integrate new factory for HAL

--- src/main/java/oshi/SystemInfo.java
+++ src/main/java/oshi/SystemInfo.java
@@ -1,13 +1,13 @@
 /**
  * Oshi (https://github.com/dblock/oshi)
- * 
+ *
  * Copyright (c) 2010 - 2016 The Oshi Project Team
- * 
+ *
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
  * http://www.eclipse.org/legal/epl-v10.html
- * 
+ *
  * Contributors:
  * dblock[at]dblock[dot]org
  * alessandro[at]perucchi[dot]org
@@ -23,9 +23,7 @@ import javax.json.JsonObject;
 import com.sun.jna.Platform;
 
 import oshi.hardware.HardwareAbstractionLayer;
-import oshi.hardware.platform.linux.LinuxHardwareAbstractionLayer;
-import oshi.hardware.platform.mac.MacHardwareAbstractionLayer;
-import oshi.hardware.platform.windows.WindowsHardwareAbstractionLayer;
+import oshi.hardware.HardwareAbstractionLayerFactory;
 import oshi.json.NullAwareJsonObjectBuilder;
 import oshi.json.OshiJsonObject;
 import oshi.software.os.OperatingSystem;
@@ -42,81 +40,73 @@ import oshi.software.os.windows.WindowsOperatingSystem;
  * @author dblock[at]dblock[dot]org
  */
 public class SystemInfo implements OshiJsonObject {
-    private OperatingSystem _os = null;
 
-    private HardwareAbstractionLayer _hardware = null;
+	private OperatingSystem _os = null;
 
-    private PlatformEnum currentPlatformEnum;
+	private HardwareAbstractionLayer _hardware = null;
 
-    {
-        if (Platform.isWindows()) {
-            this.currentPlatformEnum = PlatformEnum.WINDOWS;
-        } else if (Platform.isLinux()) {
-            this.currentPlatformEnum = PlatformEnum.LINUX;
-        } else if (Platform.isMac()) {
-            this.currentPlatformEnum = PlatformEnum.MACOSX;
-        } else {
-            this.currentPlatformEnum = PlatformEnum.UNKNOWN;
-        }
-    }
+	private final PlatformEnum _currentPlatformEnum;
 
-    private JsonBuilderFactory jsonFactory = Json.createBuilderFactory(null);
+	/**
+	 * Default constructor
+	 */
+	public SystemInfo() {
+		if (Platform.isWindows()) {
+			_currentPlatformEnum = PlatformEnum.WINDOWS;
+		} else if (Platform.isLinux()) {
+			_currentPlatformEnum = PlatformEnum.LINUX;
+		} else if (Platform.isMac()) {
+			_currentPlatformEnum = PlatformEnum.MACOSX;
+		} else {
+			_currentPlatformEnum = PlatformEnum.UNKNOWN;
+		}
+	}
 
-    /**
-     * Creates a new instance of the appropriate platform-specific
-     * {@link OperatingSystem}.
-     * 
-     * @return A new instance of {@link OperatingSystem}.
-     */
-    public OperatingSystem getOperatingSystem() {
-        if (this._os == null) {
-            switch (this.currentPlatformEnum) {
+	private JsonBuilderFactory jsonFactory = Json.createBuilderFactory(null);
 
-            case WINDOWS:
-                this._os = new WindowsOperatingSystem();
-                break;
-            case LINUX:
-                this._os = new LinuxOperatingSystem();
-                break;
-            case MACOSX:
-                this._os = new MacOperatingSystem();
-                break;
-            default:
-                throw new RuntimeException("Operating system not supported: " + Platform.getOSType());
-            }
-        }
-        return this._os;
-    }
+	/**
+	 * Creates a new instance of the appropriate platform-specific
+	 * {@link OperatingSystem}.
+	 * 
+	 * @return A new instance of {@link OperatingSystem}.
+	 */
+	public OperatingSystem getOperatingSystem() {
+		if (_os == null) {
+			switch (_currentPlatformEnum) {
 
-    /**
-     * Creates a new instance of the appropriate platform-specific
-     * {@link HardwareAbstractionLayer}.
-     * 
-     * @return A new instance of {@link HardwareAbstractionLayer}.
-     */
-    public HardwareAbstractionLayer getHardware() {
-        if (this._hardware == null) {
-            switch (this.currentPlatformEnum) {
+			case WINDOWS:
+				_os = new WindowsOperatingSystem();
+				break;
+			case LINUX:
+				_os = new LinuxOperatingSystem();
+				break;
+			case MACOSX:
+				_os = new MacOperatingSystem();
+				break;
+			default:
+				throw new RuntimeException("Operating system not supported: "
+						+ Platform.getOSType());
+			}
+		}
+		return _os;
+	}
 
-            case WINDOWS:
-                this._hardware = new WindowsHardwareAbstractionLayer();
-                break;
-            case LINUX:
-                this._hardware = new LinuxHardwareAbstractionLayer();
-                break;
-            case MACOSX:
-                this._hardware = new MacHardwareAbstractionLayer();
-                break;
-            default:
-                throw new RuntimeException("Operating system not supported: " + Platform.getOSType());
-            }
-        }
-        return this._hardware;
-    }
+	/**
+	 * Creates a new instance of the appropriate platform-specific
+	 * {@link HardwareAbstractionLayer}.
+	 * 
+	 * @return A new instance of {@link HardwareAbstractionLayer}.
+	 */
+	public HardwareAbstractionLayer getHardware() {
+		return HardwareAbstractionLayerFactory
+				.createInstance(_currentPlatformEnum);
+	}
 
-    @Override
-    public JsonObject toJSON() {
-        return NullAwareJsonObjectBuilder.wrap(jsonFactory.createObjectBuilder()).add("operatingSystem", getOperatingSystem().toJSON())
-                .add("hardware", getHardware().toJSON()).build();
-    }
+	@Override
+	public JsonObject toJSON() {
+		return NullAwareJsonObjectBuilder
+				.wrap(jsonFactory.createObjectBuilder())
+				.add("operatingSystem", getOperatingSystem().toJSON())
+				.add("hardware", getHardware().toJSON()).build();
+	}
 }
