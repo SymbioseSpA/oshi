d7943af010117669e2273daba6b0c8670bb9229c
Francisco Collao Garate <francisco.collao@symbiose.cl>
2016-04-07 11:48:21 -0300

    ADD: factory to create instances of HAL

---
+++ src/main/java/oshi/hardware/HardwareAbstractionLayerFactory.java
@@ -0,0 +1,57 @@
+/**
+ * Oshi (https://github.com/dblock/oshi)
+ *
+ * Copyright (c) 2010 - 2016 The Oshi Project Team
+ *
+ * All rights reserved. This program and the accompanying materials are made
+ * available under the terms of the Eclipse Public License v1.0 which
+ * accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ * <ul>
+ * <li>dblock[at]dblock[dot]org
+ * <li>alessandro[at]perucchi[dot]org
+ * <li>widdis[at]gmail[dot]com
+ * </ul>
+ * https://github.com/dblock/oshi/graphs/contributors
+ */
+package oshi.hardware;
+
+import com.sun.jna.Platform;
+
+import oshi.PlatformEnum;
+import oshi.hardware.platform.linux.LinuxHardwareAbstractionLayer;
+import oshi.hardware.platform.mac.MacHardwareAbstractionLayer;
+import oshi.hardware.platform.windows.WindowsHardwareAbstractionLayer;
+
+/**
+ * Factory to create instances of {@link HardwareAbstractionLayer}
+ * 
+ * @author pcollaog
+ * @since 2.3
+ */
+public class HardwareAbstractionLayerFactory {
+
+	/**
+	 * @param platform
+	 * @return HAL instance
+	 */
+	public static final HardwareAbstractionLayer createInstance(
+			PlatformEnum platform) {
+
+		switch (platform) {
+		case WINDOWS:
+			return new WindowsHardwareAbstractionLayer();
+		case LINUX:
+			return new LinuxHardwareAbstractionLayer();
+		case MACOSX:
+			return new MacHardwareAbstractionLayer();
+		default:
+			throw new RuntimeException(
+					"Operating system not supported: " + Platform.getOSType());
+		}
+
+	}
+
+}
